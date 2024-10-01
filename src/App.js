581f83314fd4bf6732c37c221492d5f5eef4e0c0
Oniichan3k <nguyenduchip1@gmail.com>
2024-10-01 17:18:29 +0700

    lab3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,184 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Container,
+  Card,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Carousel,
+  Row,
+  Col,
+  Badge,
+  Modal
+} from "react-bootstrap";
+import menu1 from "./images/menu1.jpg";
+import menu2 from "./images/menu2.jpg";
+import menu3 from "./images/menu3.jpg";
+import menu4 from "./images/menu4.jpg";
+import pizza1 from "./images/pizza1.jpg";
+import pizza2 from "./images/pizza2.jpg";
+import pizza3 from "./images/pizza3.jpg";
+import pizza4 from "./images/pizza4.jpg";
+import pizza5 from "./images/pizza5.jpg";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">About us</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Contact
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+              <Button variant="primary" onClick={setShow}>
+      Cart <Badge bg="secondary">0</Badge>
+      <span className="visually-hidden">unread messages</span>
+    </Button>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+      <Carousel>
+        <Carousel.Item>
+          <img src={pizza1} text="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={pizza2} text="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={pizza3} text="Third slide" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={pizza4} text="Third slide" />
+          <Carousel.Caption>
+            <h3>Fourth slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={pizza5} text="Third slide" />
+          <Carousel.Caption>
+            <h3>Fifth slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Row className="mt-5">
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={menu1} />
+            <Card.Body>
+              <Card.Title>Card 1</Card.Title>
+              <Card.Text>A</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={menu2} />
+            <Card.Body>
+              <Card.Title>Card 2</Card.Title>
+              <Card.Text>B</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={menu3} />
+            <Card.Body>
+              <Card.Title>Card 3</Card.Title>
+              <Card.Text>C</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={menu4} />
+            <Card.Body>
+              <Card.Title>Card 4</Card.Title>
+              <Card.Text>D</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Sucess!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </Row>
+    </>
   );
 }
 
